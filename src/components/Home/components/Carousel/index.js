2fa2c425c9e9d86733b84b82619623e5e81cf466
KJ Wilson <yourboykjdev@gmail.com>
2020-06-12 02:21:00 -0700

    added icons and new carousel

--- src/components/Home/components/Carousel/index.js
+++ src/components/Home/components/Carousel/index.js
@@ -1,28 +1,92 @@
 import React from "react";
-// import { Flickity } from "flickety";
+import Flickity from "flickity-imagesloaded";
+
 import "./styles.css";
 import houseParty from "../../../../houseparty.jpg";
 import girlSings from "../../../../girl_sing.jpg";
-import missuri from "../../../../missuri.jpg";
+import missuricrop from "../../../../missuricrop.jpg";
 import rap from "../../../../rap.jpg";
 
-// Prop: array of image links
-const Carousel = ({ images }) => {
-  return (
-    // options on main-carousel can be set using:
-    // data-flickity='{ "cellAlign": "left", "contain": true }'
-    <div
-      className="carousel"
-      data-flickity='{ "wrapAround": true, "autoPlay": 5000, "pauseAutoPlayOnHover": false, "friction": 0.6, "imagesLoaded": true}'
-    >
-      {/* TODO: map images to carousel cells */}
-      <img className="carousel-cell" src={houseParty} />
-      <img className="carousel-cell" src={girlSings} />
-      <img className="carousel-cell" src={missuri} />
-      <img className="carousel-cell" src={rap} />
-      {/* <div className="carousel-cell"></div> */}
-    </div>
-  );
-};
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mount: false,
+    };
+  }
+
+  // Prop: array of image links
+  componentDidMount() {
+    this.setState({ mount: true });
+  }
+
+  render() {
+    if (this.state.mount === true) {
+      var carousel = document.querySelector(".carousel");
+      console.log(carousel);
+      var flkty = new Flickity(carousel, {
+        imagesLoaded: true,
+        percentPosition: false,
+        autoPlay: 3500,
+        wrapAround: true,
+        pageDots: false,
+      });
+
+      var imgs = carousel.querySelectorAll(".carousel-cell img");
+      // get transform property
+      var docStyle = document.documentElement.style;
+      var transformProp =
+        typeof docStyle.transform == "string" ? "transform" : "WebkitTransform";
+
+      flkty.on("scroll", function () {
+        flkty.slides.forEach(function (slide, i) {
+          // var img = imgs[i];
+          // var x = ((slide.target + flkty.x) * -1) / 3;
+          // img.style[transformProp] = "translateX(" + x + "px)";
+
+          var img = imgs[i],
+            x = 0;
+
+          if (0 === i) {
+            x =
+              Math.abs(flkty.x) > flkty.slidesWidth
+                ? flkty.slidesWidth +
+                  flkty.x +
+                  flkty.slides[flkty.slides.length - 1].outerWidth +
+                  slide.target
+                : slide.target + flkty.x;
+          } else if (i === flkty.slides.length - 1) {
+            x =
+              Math.abs(flkty.x) + flkty.slides[i].outerWidth < flkty.slidesWidth
+                ? slide.target -
+                  flkty.slidesWidth +
+                  flkty.x -
+                  flkty.slides[i].outerWidth
+                : slide.target + flkty.x;
+          } else {
+            x = slide.target + flkty.x;
+          }
+          img.style[transformProp] = "translateX(" + x * (-1 / 3) + "px)";
+        });
+      });
+    }
+    return (
+      <div className="carousel">
+        <div className="carousel-cell">
+          <img src={houseParty} alt="houseParty" />
+        </div>
+        <div className="carousel-cell">
+          <img src={girlSings} alt="girlSings" />
+        </div>
+        <div className="carousel-cell">
+          <img src={missuricrop} alt="missuri" />
+        </div>
+        <div className="carousel-cell">
+          <img src={rap} alt="rap" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,13 @@ import "./styles.css";
 import girlSing from "../../girl_sing.jpg";
 import Carousel from "./components/Carousel";
 import Post from "../../components/Post";
+import {
+  faInstagram,
+  faFacebook,
+  faTwitter,
+  faSpotify,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Home extends Component {
   render() {
@@ -12,7 +19,27 @@ class Home extends Component {
       <div className="home">
         {/* <Logo picture={keenLogo} /> */}
         <Carousel />
-        <Post title="FIRST POST" picture={girlSing} />
+        {/* <Post title="FIRST POST" picture={girlSing} /> */}
+        <div className="icons">
+          <a target="_blank" href="https://www.instagram.com/keenmusicgroup/">
+            <FontAwesomeIcon className="icon" icon={faInstagram} size="2x" />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.facebook.com/Keen-Media-115002463374522/"
+          >
+            <FontAwesomeIcon className="icon" icon={faFacebook} size="2x" />
+          </a>
+          <a target="_blank" href="https://twitter.com/keenmusicgroup">
+            <FontAwesomeIcon className="icon" icon={faTwitter} size="2x" />
+          </a>
+          <a
+            target="_blank"
+            href="https://open.spotify.com/artist/5GITUEMmJSSaCT3rr4pQXq"
+          >
+            <FontAwesomeIcon className="icon" icon={faSpotify} size="2x" />
+          </a>
+        </div>
       </div>
     );
   }
