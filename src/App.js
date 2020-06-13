57856677ef5858329706dc831fa3e2de3e6c1751
Anisha Jain <anishajain@outlook.com>
2020-06-12 23:56:06 -0700

    subscribe button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Team from "./components/Team";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
+const THEME = createMuiTheme({
+  typography: {
+    fontFamily: `sans-serif`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/team">
-          <Team />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={THEME}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/team">
+            <Team />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
